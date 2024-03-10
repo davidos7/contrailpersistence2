282caf914297528fea75f400cfd1fd7239f6f524
David Barton <david.barton.0400@gmail.com>
2024-03-10 23:46:57 +0000

    second commit

---
+++ src/assets/tooltip.js
@@ -0,0 +1,5 @@
+window.dccFunctions = window.dccFunctions || {};
+window.dccFunctions.intToMonth = function(value) {
+    months = ["Averaged across all Months","January","February","March","April","May","June","July","August","September","October","November","December"]
+     return months[value];
+}
